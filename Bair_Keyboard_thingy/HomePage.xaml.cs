55468dcd5e000403707c7e8c93f642445a552cae
Anthony <72553201+backAir@users.noreply.github.com>
2026-01-07 05:06:31 +0100

    vibe coded UI abomination

--- Bair_Keyboard_thingy/HomePage.xaml.cs
+++ Bair_Keyboard_thingy/HomePage.xaml.cs
@@ -220,5 +220,44 @@ namespace Bair_Keyboard_thingy
         {
 
         }
+
+
+
+
+
+        // Drag + double-click maximize
+        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                ToggleMaximize();
+            }
+            else
+            {
+                DragMove();
+            }
+        }
+
+        private void Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Maximize_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleMaximize();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ToggleMaximize()
+        {
+            WindowState = WindowState == WindowState.Maximized
+                ? WindowState.Normal
+                : WindowState.Maximized;
+        }
     }
 }
