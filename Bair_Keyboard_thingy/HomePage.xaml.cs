e8b743715498e49653ca6264c414601956df230c
Anthony <72553201+backAir@users.noreply.github.com>
2026-01-09 00:40:51 +0100

    better UI

--- Bair_Keyboard_thingy/HomePage.xaml.cs
+++ Bair_Keyboard_thingy/HomePage.xaml.cs
@@ -1,21 +1,33 @@
-﻿using System.Drawing;
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-using System.Drawing;
-using System.Windows.Forms;
-using System.Diagnostics;
-
 namespace Bair_Keyboard_thingy
 {
+    public class KeyboardItem
+    {
+        public string Name { get; set; }
+        public bool IsEnabled { get; set; }
+    }
+
+    public class ProgramItem
+    {
+        public string Name { get; set; }
+        public bool IsEnabled { get; set; }
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -37,18 +49,30 @@ namespace Bair_Keyboard_thingy
         {
             QMK_API.QMK_HID keyboard = new(vendorID, productID, name, layer_count);
             keyboard.MessageReceived += OnMessageReceived;
+            KeyboardList.Items.Add(new KeyboardItem
+            {
+                Name = name,
+                IsEnabled = true
+            });
             keyboards.Add(name, keyboard);
+        }
+
+        public void UpdateKeyboardList()
+        {
+            //KeyboardList.Items.Add("My Keyboard 1");
+            //KeyboardList.Items.Add("My Keyboard 2");
         }
 
         public MainWindow()
         {
             InitializeComponent();
+            UpdateKeyboardList();
             this.Closing += MainWindow_Closing!; // event for when we close the window
             //this.Hide();
 
-            //Add_Keyboard(0x7C92, 0x0001, "pedal", 4);
-            //Add_Keyboard(0x7C92, 0x0002, "trippel_pedal", 4);
-            //Add_Keyboard(0x7C92, 0x0003, "numpad", 4);
+            Add_Keyboard(0x7C92, 0x0001, "pedal", 4);
+            Add_Keyboard(0x7C92, 0x0002, "trippel_pedal", 4);
+            Add_Keyboard(0x7C92, 0x0003, "numpad", 4);
             LoadConfig();
 
             // Optional: double-click on tray icon to restore window
@@ -123,36 +147,6 @@ namespace Bair_Keyboard_thingy
         {
             LAYER_CHANGE = 20
         }
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            var message = new byte[33];
-
-            message[0] = 1;
-            //if (HelloButton.IsChecked == true)
-            //{
-            //    message[1] = 0xAA;
-            //}
-            //else
-            //{
-            //    message[1] = 0xAB;
-            //}
-
-            //! fucked up id's: 01, 02, 04, 0C, 0D, 0E, 11, 12
-            message[1] = ((byte)PC_TO_HID_IDs.LAYER_CHANGE);
-            //message[2] = (byte)(HelloButton.IsChecked == true? 1 : 2);
-
-
-            //if (HelloButton.IsChecked == true)
-            //{
-            //    MessageBox.Show("Hello.");
-            //}
-            //else if (GoodbyeButton.IsChecked == true)
-            //{
-            //    MessageBox.Show("Goodbye.");
-            //}
-            //_notifyIcon.ShowBalloonTip(1000);
-        }
-
 
         private void ChangeLayer(QMK_API.QMK_HID keyboard, int layer)
         {
@@ -212,52 +206,22 @@ namespace Bair_Keyboard_thingy
 
         private void Setup_Dropdown()
         {
-            KeyboardDropdown.Items.Clear();
-            KeyboardDropdown.Items.Add("Pedal");
-            KeyboardDropdown.Items.Add("TrippelPedal");
+            //KeyboardDropdown.Items.Clear();
+            //KeyboardDropdown.Items.Add("Pedal");
+            //KeyboardDropdown.Items.Add("TrippelPedal");
         }
-        private void KeyboardDropdown_SelectionChanged(object sender, SelectionChangedEventArgs e)
+
+        private void KeyboardList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
 
+        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => DragMove();
+        //private void Minimize_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+        private void Minimize_Click(object sender, RoutedEventArgs e) => Close();
+        private void Maximize_Click(object sender, RoutedEventArgs e) => WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        private void Close_Click(object sender, RoutedEventArgs e) => Close();
 
 
-
-
-        // Drag + double-click maximize
-        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ClickCount == 2)
-            {
-                ToggleMaximize();
-            }
-            else
-            {
-                DragMove();
-            }
-        }
-
-        private void Minimize_Click(object sender, RoutedEventArgs e)
-        {
-            WindowState = WindowState.Minimized;
-        }
-
-        private void Maximize_Click(object sender, RoutedEventArgs e)
-        {
-            ToggleMaximize();
-        }
-
-        private void Close_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
-        }
-
-        private void ToggleMaximize()
-        {
-            WindowState = WindowState == WindowState.Maximized
-                ? WindowState.Normal
-                : WindowState.Maximized;
-        }
     }
 }
